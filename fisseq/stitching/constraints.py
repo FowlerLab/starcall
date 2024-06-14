932be74d3bbb0f9cd545d972bed8e51c89e409c9
Nicholas Bradley <nbradley@uw.edu>
2024-06-14 11:53:38 -0700

    forgot to commit constraints

---
+++ fisseq/stitching/constraints.py
@@ -0,0 +1,12 @@
+from typing import Optional
+import dataclasses
+
+
+@dataclasses.dataclass
+class Constraint:
+    dx: int
+    dy: int
+    score: Optional[float] = None
+    modeled: bool = False
+    error: int = 0
+
