fixed constraint calculation using previous

fisseq/stitching/alignment.py
@@ -1,4 +1,5 @@
 import time
+import math
 import sys
 import numpy as np
 import skimage.io
@@ -113,6 +114,10 @@ class FFTAligner(Aligner):
         if previous_constraint is not None:
             score, dx, dy, overlap = find_peaks_estimate(fft, orig_image1, orig_image2, self.num_peaks,
                     estimate=(previous_constraint.dx, previous_constraint.dy), search_range=previous_constraint.error)
+            if score == -math.inf:
+                print (dx, dy, previous_constraint.dx, previous_constraint.dy, previous_constraint.error, score)
+                return
+                #ksdfjls
         else:
             score, dx, dy, overlap = find_peaks(fft, orig_image1, orig_image2, self.num_peaks)
         constraint = Constraint(dx=dx, dy=dy, score=score, overlap=overlap)
@@ -398,7 +403,7 @@ def ncc_fast(image1, image2):
 
 @numba.jit(nopython=True)
 def find_peaks(fft, image1, image2, num_peaks):
-    best_peak = (0.0,0,0,0.0)
+    best_peak = (-math.inf,0,0,0.0)
     shape = (max(image1.shape[0], image2.shape[0]), max(image1.shape[1], image2.shape[1]))
     for i in range(num_peaks):
         peak_index = np.argmax(fft, axis=None)
@@ -428,8 +433,20 @@ def find_peaks(fft, image1, image2, num_peaks):
 
 @numba.jit(nopython=True)
 def find_peaks_estimate(fft, image1, image2, num_peaks, estimate, search_range):
-    best_peak = (0.0,0,0,0.0)
+    best_peak = (-math.inf,0,0,0.0)
     shape = (max(image1.shape[0], image2.shape[0]), max(image1.shape[1], image2.shape[1]))
+
+    for x in range(fft.shape[0]):
+        for y in range(fft.shape[1]):
+            in_range = False
+            for xval in (x, shape[0] - x):
+                for yval in (y, shape[1] - y):
+                    for xval in (xval, -xval):
+                        for yval in (yval, -yval):
+                            in_range = in_range or max(abs(estimate[0] - xval), abs(estimate[1] - yval)) <= search_range
+            if not in_range:
+                fft[x,y] = -np.inf
+
     for i in range(num_peaks):
         peak_index = np.argmax(fft, axis=None)
         xval, yval = peak_index // fft.shape[1], peak_index % fft.shape[1]
@@ -437,8 +454,10 @@ def find_peaks_estimate(fft, image1, image2, num_peaks, estimate, search_range):
             for yval in (yval, shape[1] - yval):
                 for xval in (xval, -xval):
                     for yval in (yval, -yval):
+                        #print ("  checking", xval, yval)
                         if max(abs(estimate[0] - xval), abs(estimate[1] - yval)) > search_range:
                             continue
+                        #print ("  in range")
 
                         section1 = image1[max(0,xval):,max(0,yval):]
                         section2 = image2[max(0,-xval):,max(0,-yval):]
@@ -447,6 +466,7 @@ def find_peaks_estimate(fft, image1, image2, num_peaks, estimate, search_range):
                         section1 = section1[:dim1,:dim2]
                         section2 = section2[:dim1,:dim2]
                         if section1.size == 0: continue
+                        #print ("   real")
 
                         overlap = max(section1.size / image1.size, section2.size / image2.size)
                         peak = (ncc_fast(section1, section2), xval, yval, overlap)
@@ -455,7 +475,7 @@ def find_peaks_estimate(fft, image1, image2, num_peaks, estimate, search_range):
                             best_peak = peak
 
                 if xval < shape[0] and yval < shape[1]:
-                    fft[xval,yval] = 0
+                    fft[xval,yval] = -np.inf
 
     return best_peak
 

fisseq/stitching/composite.py
@@ -701,13 +701,13 @@ class CompositeImage:
 
     def find_unconstrained_pairs(self, *args, **kwargs):
         """ Finds all pairs of images that overlap based on the estimated positions and
-        that don't already have a constraint.
+        that don't already have a constraint, or that have a constraint with an error value
 
         Returns (np.ndarray shape (N, 2)):
             The sequence of pairs of indices of the images that overlap without constraints.
         """
         pairs = self.find_pairs(*args, **kwargs)
-        mask = [(i,j) not in self.constraints for i,j in pairs]
+        mask = [(i,j) not in self.constraints or self.constraints[i,j].error > 0 for i,j in pairs]
         return pairs[mask]
 
     def prune_pairs(self, pairs):
@@ -789,7 +789,9 @@ class CompositeImage:
             ))
 
         for (index1, index2), future in self.progress(zip(pairs, futures), total=len(futures)):
-            constraints[(index1,index2)] = future.result()
+            result = future.result()
+            if result is not None:
+                constraints[(index1,index2)] = result
 
         if debug and len(constraints) != 0:
             scores = np.array([const.score for const in constraints.values()])
@@ -799,6 +801,31 @@ class CompositeImage:
             return constraints
         else:
             self.constraints.update(constraints)
+
+    def estimate_constraints(self, pairs=None, error=0.3, score=0, return_constraints=False):
+        """ Estimates constraints based on the current stage positions of the images
+        Useful if you are pretty confident in the stage positions as these constraints
+        will keep the calculated ones within a certain range of the stage position.
+        """
+        if pairs is None:
+            pairs = self.find_unconstrained_pairs()
+            print (pairs)
+
+        constraints = {}
+
+        for index1, index2 in pairs:
+            offset = self.boxes[index2].pos1 - self.boxes[index1].pos1
+            cur_error = error
+            if error < 1:
+                cur_error = int(max(*self.boxes[index1].size()[:2], *self.boxes[index2].size()[:2]) * error)
+            constraint = Constraint(dx=offset[0], dy=offset[1], score=score, error=cur_error)
+            constraints[index1,index2] = constraint
+
+        if return_constraints:
+            return constraints
+        else:
+            self.constraints.update(constraints)
+            print (constraints)
 
     def calc_score_threshold(self, num_samples=None, random_state=12345):
         """ Estimates a threshold for selecting constraints with good overlap.
@@ -960,7 +987,8 @@ class CompositeImage:
                 warnings.warn("Stage model filtered out over 20% of constraints as outilers."
                         " It may have hyperoptimized to the data, make sure all are actually outliers")
 
-            self.debug("Estimated stage model", model, "with an r2 score of", model.score(est_poses[model.inlier_mask_], const_poses[model.inlier_mask_]),
+            est_poses, const_poses = est_poses[model.inlier_mask_], const_poses[model.inlier_mask_]
+            self.debug("Estimated stage model", model, "with an r2 score of", model.score(est_poses, const_poses),
                     ", classifying {}/{} constraints as outliers".format(np.sum(~model.inlier_mask_), len(self.constraints)))
 
             for (i,j) in indices[~model.inlier_mask_]:
@@ -990,6 +1018,12 @@ class CompositeImage:
                 " it may have hyperoptimized to the training data.")
 
         self.stage_model = model
+
+        # calculate variance
+        error = model.predict(est_poses) - const_poses
+        print ("Stage model error", np.percentile(np.abs(error), [0,5,50,75,95,100]))
+        error = np.percentile(np.abs(error), 75)
+        self.stage_model_error = error
 
     def filter_outliers(self, pairs=None):
         """ Filters out any constraints that are clearly outliers, ie have a much larger magnitude than
@@ -1024,7 +1058,7 @@ class CompositeImage:
         for pair in pairs[mask]:
             del self.constraints[(pair[0], pair[1])]
 
-    def model_constraints(self, pairs=None, score_multiplier=0.5, return_constraints=False):
+    def model_constraints(self, pairs=None, score_multiplier=0.5, use_stage_model_error=True, error=0, return_constraints=False):
         """ Uses the stored stage model (estimated by estimage_stage_model) to
         fill the specified constraints.
             
@@ -1056,6 +1090,9 @@ class CompositeImage:
         if self.stage_model is None:
             self.estimate_stage_model()
 
+        if use_stage_model_error:
+            error = self.stage_model_error
+
         constraints = {} if return_constraints else self.constraints
 
         start_size = len(constraints)
@@ -1067,7 +1104,7 @@ class CompositeImage:
                 " The stage model may not have correctly modeled the movement")
             #score = score_offset(self.images[i], self.images[j], dx, dy) * score_multiplier
             score = 0.2
-            constraints[(i,j)] = Constraint(score=score, dx=dx, dy=dy, modeled=True)
+            constraints[(i,j)] = Constraint(score=score, dx=dx, dy=dy, modeled=True, error=error)
 
         self.debug('Added', len(constraints) - start_size, 'calculated constraints using stage model')
 
@@ -1132,7 +1169,7 @@ class CompositeImage:
 
         return solution_mat, solution_vals
 
-    def solve_constraints(self, constraints=None, solver=None, apply_positions=True, ignore_bad_constraints=False):
+    def solve_constraints(self, constraints=None, solver=None, apply_positions=True, ignore_bad_constraints=True, filter_outliers=True):
         """ Solves all contained constraints to get absolute image positions.
 
         This is done by constructing a set of linear equations, with every constraint
@@ -1149,19 +1186,14 @@ class CompositeImage:
             Returns: np.ndarray shape (len(images), n_dims)
                 The solved positions of images.
         """
-        solver = solver or solving.OutlierSolver()
+        solver = solver or solving.LinearSolver()
         constraints = constraints or self.constraints.copy()
-        #constraints = dict(list(constraints.items())[:20])
-        #def in_range(val):
-            #return 5 <= val % 23 <= 10 and 6 <= val // 23 <= 11
-        #constraints = {pair: const for pair, const in constraints.items() if in_range(pair[0]) and in_range(pair[1])}
-        #constraints = {pair: const for pair, const in constraints.items() if pair[1] < 20}
-        result = solver.solve(constraints)
+        result = solver.solve(constraints, dict(zip(range(len(self.boxes)), self.boxes.pos1)))
 
         if type(result) == tuple and len(result) == 2:
-            poses, inlier_mask = result
-            for pair in np.array(list(constraints.keys()))[~np.asarray(inlier_mask)]:
-                del constraints[pair]
+            poses, constraints = result
+            if filter_outliers:
+                self.constraints = constraints
         else:
             poses = result
 
@@ -1438,6 +1470,9 @@ class CompositeImage:
         groups = [list(range(len(self.boxes)))]
         const_groups = [self.constraints.keys()]
         names = ['']
+        if score_func == 'accuracy':
+            score_func = self.constraint_accuracy
+
         if self.boxes[0].pos1.shape[0] == 3:
             groups = []
             const_groups = []

fisseq/stitching/solving.py
@@ -3,6 +3,7 @@ import time
 import numpy as np
 import sklearn.linear_model
 import skimage.io
+import scipy.optimize
 
 from .constraints import Constraint
 
@@ -11,13 +12,12 @@ class Solver:
     and solves them into global positions
     """
 
-    def solve(self, constraints):
-        """ Solve the global positions for images given the constraints.
+    def solve(self, constraints, initial_poses):
+        """ Solve the global positions for images given the constraints and estimated positions.
         returns the xy position for each image, mapping the image index to the position
         with a dictionary.
-        Additionally a boolean mask can be returned as a second parameter
-        that specifies the constraints that were classified as inliers,
-        and should be kept.
+        Additionally the constraints dictionary can be returned as the second return value, which
+        will replace the constraints in the composite with the ones returned, filtering outliers.
         """
         pass
 
@@ -30,9 +30,8 @@ class LinearSolver:
     def __init__(self, model=None):
         self.model = model or sklearn.linear_model.LinearRegression(fit_intercept=False)
 
-    #def make_constraint_matrix(self, constraints):
-    def solve(self, constraints):
-        image_indices = sorted(list(set(pair[0] for pair in constraints) | set(pair[1] for pair in constraints)))
+    def make_constraint_matrix(self, constraints, initial_poses):
+        image_indices = sorted(list(initial_poses.keys()))
         solution_mat = np.zeros((len(constraints)*2+2, len(image_indices)*2))
         solution_vals = np.zeros(len(constraints)*2+2)
         
@@ -53,14 +52,22 @@ class LinearSolver:
         solution_mat[-2, 0] = 1
         solution_mat[-1, 1] = 1
 
-        solution = self.solve_matrix(solution_mat, solution_vals)
+        initial_values = np.array(list(initial_poses.values()))
+
+        return solution_mat, solution_vals, initial_values
+
+    def solve(self, constraints, initial_poses):
+        #image_indices = sorted(list(set(pair[0] for pair in constraints) | set(pair[1] for pair in constraints)))
+        solution_mat, solution_vals, initial_values = self.make_constraint_matrix(constraints, initial_poses)
+
+        solution = self.solve_matrix(solution_mat, solution_vals, initial_values)
 
         poses = np.round(solution.reshape(-1,2)).astype(int)
         poses -= poses.min(axis=0).reshape(1,2)
 
-        return dict(zip(image_indices, poses))
+        return dict(zip(list(initial_poses.keys()), poses))
 
-    def solve_matrix(self, solution_mat, solution_vals):
+    def solve_matrix(self, solution_mat, solution_vals, initial_values):
         #solution, residuals, rank, sing = np.linalg.lstsq(solution_mat, solution_vals, rcond=None)
         model = self.model.fit(solution_mat, solution_vals)
         solution = model.coef_
@@ -68,6 +75,46 @@ class LinearSolver:
 
     def score_func(self, constraint):
         return max(0, constraint.score) + 0.1
+
+class OptimalSolver(LinearSolver):
+    def __init__(self, **kwargs):
+        super().__init__()
+
+    def solve_matrix(self, solution_mat, solution_vals, initial_values):
+        values = initial_values
+        initial_poses = values.reshape(-1, 2)
+
+        def simple_loss(values):
+            error = np.matmul(solution_mat, values.T) - solution_vals
+            error = 1 / (1 + np.exp(-error / 25)) - 0.5
+            return np.sum(np.abs(error))
+            #return np.sum(error * error)
+
+        result = scipy.optimize.minimize(simple_loss, values)#, options=dict(maxiter=10))
+        values = result.x
+        np.save('tmp_values.npy', values)
+
+        """
+        import matplotlib.pyplot as plt
+        #fig, axes = plt.subplots(ncol=1, nrow=20, figsize=(6, 4*20))
+        fig, axis = plt.subplots(figsize=(15, 15))
+        all_poses = [initial_poses]
+        for i in range(1):
+            print ('optimizing')
+            result = scipy.optimize.minimize(simple_loss, values)#, options=dict(maxiter=10))
+            values = result.x
+            poses = values.reshape(-1, 2)
+            all_poses.append(poses)
+            #axis.scatter(poses[:,0], poses[:,1], s=1)
+        all_poses = np.array(all_poses)
+
+        axis.scatter(all_poses[0,:,0], all_poses[0,:,1])
+        for i in range(all_poses.shape[1]):
+            axis.plot(all_poses[:,i,0], all_poses[:,i,1])
+        fig.savefig('plots/huber_optim.png')
+        """
+
+        return values
 
 class OutlierSolver:
     def __init__(self, solver=None, testing_radius=3):