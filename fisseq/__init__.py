4b3723f86933d59f4d914b017c55e29c41d94f84
Nicholas Bradley <nbradley@uw.edu>
2024-12-21 01:50:22 -0800

    added stitching as separate package, contained in fisseq to make transition easier

--- fisseq/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import dotdetection, alignment, stitching, segmentation, utils, correction, old_stitching, io, sequencing
-
-__all__ = ['dotdetection', 'alignment', 'stitching', 'segmentation', 'utils', 'correction', 'old_stitching', 'io', 'sequencing']
-
---
+++ fisseq/stitching.py
@@ -0,0 +1,1 @@
+from .constitch.constitch import *
